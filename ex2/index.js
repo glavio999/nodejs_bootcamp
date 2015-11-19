a775216ad2eb2759f815858fe5aa11bbcbe27c78
8108_web (Student) <s_8108_web@PEDAGOGIQUE.LAN>
2015-11-19 14:30:53 +0100

    nfbjdfg

--- ex2/index.js
+++ ex2/index.js
@@ -2,8 +2,8 @@ var fs = require('fs');
 fs.readFile('fake.txt', function(err, data) {
   if (err) throw err;
   // console.log(data.toString());
-  // console.log(data.toString().split('\n').length);
   var inData = data.toString();
   var lineCount = inData.split('\n').length;
   console.log(lineCount);
+  // console.log(data.toString().split('\n').length);
 });
