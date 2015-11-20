44973f346dc994f0d8c063e34c74ca60d6191946
8108_web (Student) <s_8108_web@PEDAGOGIQUE.LAN>
2015-11-20 14:40:57 +0100

    j'aime les licornes

---
+++ ex7/index.js
@@ -0,0 +1,26 @@
+var http = require('http');
+var fs = require('fs');
+//Create an HTTP server
+
+var routes ={
+  "/": 'templates/index.html',
+  "/page1":"templates/page1.html"
+}
+var srv = http.createServer(function (req, res){
+  console.log(req.url);
+  if(routes[req.url]) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var routeFile = routes[req.url];
+    fs.readFile(routeFile, function(err,data){
+      if (err) throw err
+      res.end(data);
+
+    })
+  } else {
+    res.writeHead(404);
+    res.end('the url :'+req.url +" don't exist");
+  }
+});
+srv.listen(3000, function(err){
+  console.log("servee is now listening on port 3000");
+});
